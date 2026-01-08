253ecec093d665947d3800f3a83053a4636cf3c0
ahmed magdy <ahmedmagdee04@gmail.com>
2026-01-08 20:09:45 +0200

    feat: implement user registration controller

---
+++ server/controllers/userControllers.js
@@ -0,0 +1,41 @@
+import { User } from "../models/userModel";
+
+export const register = async (req, res) => {
+    try {
+        const {firstName, lastName, email, password} = req.body;
+        if(!firstName || !lastName || !email || !password) {
+            res.status(400).json({
+                success: false,
+                message: `All fields are required`
+            })
+        }
+
+        const user = await User.findOne({email});
+        if(user) {
+            res.status(400).json({
+                success: false,
+                message: `User already exist`
+            })
+        }
+
+        const newUser = await User.create({
+            firstName,
+            lastName,
+            email,
+            password
+        });
+
+        await newUser.save();
+        return res.status(201).json({
+            success: true,
+            message: 'User registered Successfully',
+            user: newUser
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message  
+        })
+    }
+}
